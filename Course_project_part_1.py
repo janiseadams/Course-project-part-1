41afabd18a1dfd62613c936f69f055aca35c50e3
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-19 17:25:56 -0800

    Working code

--- Course_project_part_1.py
+++ Course_project_part_1.py
@@ -1,66 +1,65 @@
 
 
-def employeename():
-    empname = input("Enter employee name: ")
+def GetEmpName():
+    empname = input("Enter Employee name: ")
     return empname
 
-def hours():
-    totalhours = float(input("Enter toal hours worked: "))
+def hoursworked():
+    totalhours = float(input("Enter Total hours worked: "))
     return totalhours
 
-def rate():
-    hourlyrate = float(input("Enter hourly rate: "))
+def getrate():
+    hourlyrate = float(input("Enter Hourly rate: "))
     return hourlyrate
 
 def incometaxrate():
-    income = float(input("Enter Income tax rate: "))
-    return income
+    incometax = float(input("Enter Income tax rate: "))
+    return incometax
 
-def grosstaxnet(totalhours, hourlyrate, taxrate):
-    gross = totalhours*hourlyrate
-    income = gross*taxrate
-    net = gross - income
-    return gross, income, net
+def gettaxandnet(totalhours, hourlyrate, taxrate):
+    grosspay = totalhours * hourlyrate
+    incometax = grosspay * taxrate
+    netpay = grosspay - incometax
+    return grosspay, incometax, netpay
 
-def printing(empname, hours, hourlyrate, gross, taxrate, income, net):
-    print("Name: ", empname)
-    print("Hours worked: ",f"{hours:..2f}")
-    print("Hourly rate: f{hourlyrate:..2f}")
-    print("Gross pay: f{gross:..2f}")
-    print("Tax rate: f{taxrate:..0%}")
-    print("Income pay: f{income:..2f}")
-    print("Net pay: f{net:..2f}")
+def printinfo(empname, totalhours, hourlyrate, grosspay, taxrate, incometax, netpay):
+    print("Name:  ", empname) 
+    print("Hours Worked: {:.2f}".format(totalhours))
+    print("Hourly rate: {:.2f}".format(hourlyrate))
+    print("Total Gross Pay: {:.2f}".format(grosspay))
+    print("Total Tax Rate: {:.0%}".format(taxrate))
+    print("Income Tax Rate : {:.2f}".format(incometax))
+    print("Total Net Pay: {:.2f}".format(netpay))
+    print()
 
-def otherprinting(totalemp, totalhours, totalgrosspay, totaltax, totalnetpay):
-    print(f"Total number of employees: {totalemp}")
-    print(f"Total number of hours worked: {totalhours:..2f}")
-    print(f"Total gross pay: {totalgrosspay:..2f}")
-    print(f"Total tax: {totaltax:..2f}")
-    print(f"Total netpay: {totalnetpay:..2f}")
+def PrintTotals(empname, TotHours, TotGrossPay, TotTax, TotNetPay):    
+    print()
+    print("Total Number Of Employees: {TotEmployees}")
+    print("Total Hours Worked: {:.2f}".format(TotHours))
+    print("Total Gross Pay: {:.2f}".format(TotGrossPay))
+    print("Total Income Tax Rate: {:.0%}".format(TotTax))
+    print("Total Net Pay: {:.2f}".format(TotNetPay))
+
 
 if __name__ == "__main__":
-    totalemp = 0
-    totalhours = 0.0
-    totalgrosspay = 0.0
-    totaltax = 0.0
-    totalnetpay = 0.0
-    while true:
-        empname = employeename()
+    TotEmployees = 0
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
+    while True:
+        empname = GetEmpName()
         if (empname.upper() == "END"):
             break
-        totalhours = hours()
-        hourlyrate = rate()
+        totalhours = hoursworked()
+        hourlyrate = getrate()
         taxrate = incometaxrate()
-        gross, income, totalnetpay = grosstaxnet(totalhours, hourlyrate, taxrate)
-        printinfo(empname, hours, hourlyrate, gross, taxrate, income, netpay)
-        totalemp += 1
-        totalhours += hours
-        print(totalemp,totalhours, gross, totaltax, totalnetpay)
-    print()
-print("END")
-
-
-
-
-
-
+        grosspay, incometax, netpay = gettaxandnet(totalhours, hourlyrate, taxrate)
+        printinfo(empname, totalhours, hourlyrate, grosspay, taxrate, incometax, netpay)
+        TotEmployees += 1
+        totalhours += totalhours
+        TotGrossPay += grosspay
+        TotTax += incometax
+        TotNetPay += netpay
+    PrintTotals (TotEmployees, TotHours, TotGrossPay, TotTax, TotNetPay)
+print("Goodbye!")
